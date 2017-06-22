4297b34919deb44097608a21f1a61511fbafda81
unknown <jaylesh27@gmail.com>
2017-06-22 13:18:16 -0400

    added display function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,10 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require('cli-table2');
+
+var table = new Table({
+    head: ['Product ID', 'Product', 'Department', 'Price($)', 'Stock Qty']
+});
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -14,5 +19,19 @@ connection.connect(function (err) {
 		throw err;
 	}
 	console.log("connected as id " + connection.threadId);
-
+	displayItems();
 });
+
+function displayItems() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) {
+			throw err;
+		}
+		for (var i = 0; i < res.length; i++) {
+			table.push(
+				[res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+			);
+		}
+		console.log(table.toString());
+	});
+}
