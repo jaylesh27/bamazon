1665f4f9f2e238031fc31649345675d8ce98aced
unknown <jaylesh27@gmail.com>
2017-06-22 17:23:52 -0400

    added inquirer function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,11 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var Table = require('cli-table2');
+//var Table = require('cli-table2');
 
-var table = new Table({
-    head: ['Product ID', 'Product', 'Department', 'Price($)', 'Stock Qty']
-});
+/*var table = new Table({
+    head: ['Product ID', 'Product', 'Department', 'Price($)', 'Stock Qty'],
+    //colWidths: [15, 55, 55, 10, 10]
+}); */
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -18,8 +19,9 @@ connection.connect(function (err) {
 	if (err) {
 		throw err;
 	}
-	console.log("connected as id " + connection.threadId);
+	//console.log("connected as id " + connection.threadId);
 	displayItems();
+	purchasePrompt();
 });
 
 function displayItems() {
@@ -27,11 +29,44 @@ function displayItems() {
 		if (err) {
 			throw err;
 		}
+		console.log(
+			'Product ID' + "    ||    " +
+			'Product' + "    ||    " +
+			'Department' + "    ||    " +
+			'Price($)' + "    ||    " +
+			'Stock Qty' +
+			"\n-------------------------------------------------------------------------------------"
+		);
+
+		for (var i = 0; i < res.length; i++) {
+			console.log(
+				res[i].item_id + "    ||    " +
+				res[i].product_name + "    ||    " +
+				res[i].department_name + "    ||    " +
+				res[i].price + "    ||    " +
+				res[i].stock_quantity +
+				"\n-------------------------------------------------------------------------------------"
+			);
+		}
+		/*
 		for (var i = 0; i < res.length; i++) {
 			table.push(
 				[res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
 			);
 		}
 		console.log(table.toString());
+		*/
+	});
+}
+
+function purchasePrompt() {
+	inquirer.prompt([
+		{
+			name: "item purchase",
+			type: "input",
+			message: "Enter the ID of the item you want to buy"
+		}
+	]).then(function (answer) {
+		console.log(answer);
 	});
 }
